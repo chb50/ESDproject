71b0c557e5364c1da0c4e22903695b56dec56b70
chb50 <cedricblake48@gmail.com>
2016-12-05 19:51:18 -0500

    ADC script for potentiometer

---
+++ Unit Tests/adcTry/adcTry.ino
@@ -0,0 +1,25 @@
+//Written By: Cedric Blake
+//script to test out using the arduino uno's ADC
+//tested using a VEX Potentiometer
+//digital value ranges from 0 to 1023
+//values are displayed on the Serial Monitor
+
+
+int val = 0;
+String namae;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+  Serial.println("TEST");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(A0);
+  //printf("%d\n",val);
+  Serial.print(val);
+  Serial.print("\n");
+  delay(500);
+}
